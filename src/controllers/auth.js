3bc4e94eaceffb67a5a3291c9f2b3e5b94657f0a
Areydra <areydra@gmail.com>
2019-09-15 10:38:35 +0700

    v1.2

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken')
+const localStorage = require('local-storage')
+
 const usersModel = require('../models/users')
 const formResponse = require('../helper/formResponse')
 
@@ -6,20 +8,26 @@ module.exports = {
     login : (req, res) => {
         usersModel.getUserByEmail(req.body.email).then(response => {
             if(response.length){
-                if(response[0].password !== req.body.password) res.send('email or password invalid')
-                
-                //membuat jwt dengan ({payload}, 'secret-key', {kadaluarsa dalam : '1 jam'}) (s=second, m=minute, h=hours)
-                let token = jwt.sign({...response}, 'areydra', {expiresIn: '1h'})
+                if(response[0].password !== req.body.password){
+                    res.status(410).send('email or password isnt valid')
+                    // formResponse.success(res, 410, {error : 'email or password invalid'})
+                }
+                else{
+                    //membuat jwt dengan ({payload}, 'secret-key', {kadaluarsa dalam : '1 jam'}) (s=second, m=minute, h=hours)
+                    let token = jwt.sign({...response}, 'areydra', {expiresIn: '1h'})
 
-                // Method Date.now() mengembalikan jumlah milisekon sejak 1 Januari 1970 00: 00: 00 UTC.
-                // { token : token, dateNow: Date.now(), hashed:hashed.exp*1000 }
-                // agar format exp menjadi sama dengan date.now maka kalikan 1000
-                
-                res.set({ 'x-auth-token' : token }) //untuk mensetting nilai header dengan key 'x-auth-token' : value token
+                    // Method Date.now() mengembalikan jumlah milisekon sejak 1 Januari 1970 00: 00: 00 UTC.
+                    // { token : token, dateNow: Date.now(), hashed:hashed.exp*1000 }
+                    // agar format exp menjadi sama dengan date.now maka kalikan 1000
+                    
+                    // res.set({ 'x-auth-token' : token }) //untuk mensetting nilai header dengan key 'x-auth-token' : value token
+                    response = {...response[0], token} //membuat key baru dari response[0], dengan key token:value token
+                    delete response['password'] //menghapus response password agar tidak ditampilkan ke user
 
-                formResponse.success(res, 200, response)
+                    formResponse.success(res, 200, response)
+                }
             }else{
-                res.send('email or password invalid')
+                formResponse.success(res, 410, {error : 'email or password invalid'})
             }
         })
     }
